db1a60c5ec3d953657cdfc5136f2de7e908f9bdf
Benson Gicheru <bensongicheru@Bensons-MacBook-Pro.local>
2023-05-11 16:35:54 +0300

    Fix double export

--- jvectormap-next/src/jquery-jvectormap.js
+++ jvectormap-next/src/jquery-jvectormap.js
@@ -8,7 +8,7 @@
 (function (factory) {
   if (typeof exports === 'object') {
     // Node/CommonJS style for Browserify
-    module.exports = factory;
+    module.exports.jvectormap = factory;
   } else if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
     define(['jquery'], factory);
